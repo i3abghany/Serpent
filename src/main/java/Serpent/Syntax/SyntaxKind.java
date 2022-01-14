6033d0fce2aa14fb398b81316d8c4633738eecfb
Mahmoud Mandour <Mahmoudabdalghany@outlook.com>
2022-01-15 00:24:31 +0200

    Lexer: Add textual names in enum values.

--- src/main/java/Serpent/Syntax/SyntaxKind.java
+++ src/main/java/Serpent/Syntax/SyntaxKind.java
@@ -2,23 +2,35 @@ package Serpent.Syntax;
 
 public enum SyntaxKind {
     BadToken,
-    PlusToken,
-    MinusToken,
-    StarToken,
-    SlashToken,
-    OpenParenthesisToken,
-    CloseParenthesisToken,
+    PlusToken("+"),
+    MinusToken("-"),
+    StarToken("*"),
+    SlashToken("/"),
+    OpenParenthesisToken("("),
+    CloseParenthesisToken(")"),
     NumberToken,
     IdentifierToken,
     WhitespaceToken,
-    CaretToken,
+    CaretToken("^"),
 
-    FalseKeyword,
-    TrueKeyword,
+    FalseKeyword("false"),
+    TrueKeyword("true"),
 
-    NumberExpression,
     BinaryExpression,
+    NumberExpression,
     ParenthesizedExpression,
+    UnaryExpression,
 
-    EndOfFileToken, UnaryExpression,
+    EndOfFileToken,
+    ;
+
+    public final String text;
+
+    SyntaxKind(String text) {
+        this.text = text;
+    }
+
+    SyntaxKind() {
+        this(null);
+    }
 }
