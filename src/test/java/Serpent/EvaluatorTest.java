6645e3bcd13c384cc9999a821dbe944f11b07b05
Mahmoud Mandour <Mahmoudabdalghany@outlook.com>
2022-01-16 17:54:13 +0200

    Tests: Make arithmetic arguments both positive and negative.

--- src/test/java/Serpent/EvaluatorTest.java
+++ src/test/java/Serpent/EvaluatorTest.java
@@ -47,8 +47,8 @@ class EvaluatorTest {
         List<SyntaxKind> ops = SyntaxTraits.getArithmeticBinaryOperators();
         Stream<Arguments> s = Stream.empty();
         for (SyntaxKind op : ops) {
-            for (int i = 0; i <= 7; i++) {
-                for (int j = 0; j <= 150; j += 19) {
+            for (int i = -3; i <= 3; i++) {
+                for (int j = -60; j <= 150; j += 19) {
                     s = Stream.concat(s, Stream.of(Arguments.of(op, i, j)));
                 }
             }
